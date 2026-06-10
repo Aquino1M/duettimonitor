00d4dd2cdba22b5a53737fb85fb3cb8e1638a231
Aquino1M <diogenesdidi83@gmail.com>
2026-06-10 09:32:23 -0300

    Update rodar.bat

--- rodar.bat
+++ rodar.bat
@@ -7,7 +7,7 @@ echo        A INICIAR O ROBO DA DUETTI...
 echo ===========================================
 echo.
 
-:: Alterado para abrir o ficheiro antigo que deves ter na pasta
+:: Executa exatamente o ficheiro que alterámos acima
 python monitor_duetti.py
 
 if %ERRORLEVEL% NEQ 0 (
@@ -15,8 +15,6 @@ if %ERRORLEVEL% NEQ 0 (
     echo.
     echo ===========================================
     echo ❌ ERRO CRITICO: O ROBO FECHOU INESPERADAMENTE!
-    echo Por favor, le a mensagem de erro acima para 
-    echo perceber o que falhou no Python.
     echo ===========================================
     pause
     exit
